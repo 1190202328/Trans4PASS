0ac6ca5803c6c009b7cc9eeb3b23b7f03daf3595
JingJiang <1259029328@qq.com>
2024-01-05 11:41:18 +0800

    更新分布式运行的脚本，跑CS->DP的实验

--- dist_train.sh
+++ dist_train.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+bash /nfs/volume-902-16/tangwenbo/s3_all.sh
+
 # step 0 train on source
 # Trans4PASS
-cd /nfs/ofs-902-1/object-detection/jiangjing/experiments/Trans4PASS && CUDA_VISIBLE_DEVICES=0 /home/luban/apps/miniconda/miniconda/envs/torch1101/bin/python \
-  tools/train_cs.py --config-file configs/cityscapes/trans4pass_tiny_512x512.yaml
+cd /nfs/ofs-902-1/object-detection/jiangjing/experiments/Trans4PASS && CUDA_VISIBLE_DEVICES=0,1,2,3 /home/luban/apps/miniconda/miniconda/envs/torch1101/bin/python \
+  -m torch.distributed.launch --nproc_per_node=4 tools/train_cs.py --config-file configs/cityscapes/trans4pass_tiny_512x512.yaml
 
 ## step1 warm-up
 #cd /nfs/ofs-902-1/object-detection/jiangjing/experiments/Trans4PASS/adaptations && CUDA_VISIBLE_DEVICES=0 /home/luban/apps/miniconda/miniconda/envs/torch1101/bin/python \
