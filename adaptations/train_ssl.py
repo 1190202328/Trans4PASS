72b8ad29c07665c9a37a15aa19bd2dcc59c99eae
JingJiang <1259029328@qq.com>
2024-02-29 14:42:40 +0800

    train_mpa小修复

--- adaptations/train_ssl.py
+++ adaptations/train_ssl.py
@@ -459,7 +459,7 @@ def main():
                 torch.save(model.state_dict(),
                            osp.join(args.snapshot_dir, 'best.pth'))
                 torch.save(model_D.state_dict(),
-                           osp.join(args.snapshot_dir, 'best_D.sh'))
+                           osp.join(args.snapshot_dir, 'best_D.pth'))
                 with open(osp.join(args.snapshot_dir, 'best_miou.txt'), mode='w', encoding='utf-8') as f:
                     f.write(best_miou_str)
             unfreeze_model(model)
